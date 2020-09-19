c3c46994ad831c42007ba6295ed3e9d4ff886092
nick <njd5475@gmail.com>
2020-09-19 11:35:33 -0400

    Add config object and resolve music dirs

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 #!/usr/bin/env node
 const moment = require('moment');
 const math = require('mathjs');
+const path = require('path');
+const { loadConfig, reconfigure } = require('./lib/config');
 const momentDurationFormatSetup = require("moment-duration-format");
 momentDurationFormatSetup(moment);
 const player = require('play-sound')();
 
-const time = async function(delay) {
+const time = async function(config, delay) {
   const toHuman = (counter) => moment.duration(counter, "seconds").format("h [hrs], m [min], s [secs]")
   const TIMER = math.evaluate(delay);
   console.log(`Alarm in ${toHuman(TIMER)} seconds`);
@@ -17,7 +19,7 @@ const time = async function(delay) {
     if(counter > 0) {
       setTimeout(timeout, 1000);
     } else {
-      await player.play(config.sound);
+      await player.play(path.join(config.sound));
     }
   };
   
@@ -27,38 +29,13 @@ const time = async function(delay) {
 const { argv } = require('yargs')
   .usage('Usage: $0 [options] <exp>')
   .command('$0 <exp>', 'The number of seconds or math expression of seconds to wait.', () => {}, async ({exp}) => {
-    return await time(exp);
+    const config = await loadConfig();
+    return await time(config, exp);
   })
-  .option('config', {alias: 'c', describe: 'Configuration file for sound to play', command: '$0 <file>'})
+  .command({alias: 'c', describe: 'Configuration file for sound to play', command: 'config',
+    handler: async () => {
+      await reconfigure();
+    }
+  })
   .help('h')
   .alias('h', 'help');
-
-const config = require('./config.json');
-const yargs = require('yargs');
-
-console.log(argv);
-if(argv.config || !config.sound) {
-  inDoMode = false;
-  (async function() {
-    console.log("Config saved");
-    
-    const { Select } = require('enquirer');
-    const fs = require('fs');
-    const { promisify } = require('util');
-    const readdirp = promisify(fs.readdir);
-    const write = promisify(fs.writeFile);
-    let files = await readdirp('./music');
-    
-    const prompt = new Select({
-      name: 'value',
-      message: 'Pick a alarm sound',
-      choices: files,
-    });
-    
-    const sound = await prompt.run();
-    console.log(`Changing sound from ${config.sound} to ${sound}`);
-    config.sound = `./music/${sound}`;
-    await write("./config.json", JSON.stringify(config, null, "   "));
-    process.exit();
-  })();
-}
